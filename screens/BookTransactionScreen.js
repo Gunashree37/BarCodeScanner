ccddc051918925ef8f1f2ad36bc8a7a2ecec28f9
Gunashree S <gunashrees341@gmail.com>
2021-06-18 18:21:13 +0530

    Update BookTransactionScreen.js

--- screens/BookTransactionScreen.js
+++ screens/BookTransactionScreen.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
-import * as Permissions from 'expo-permissions';
-import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default class TransactionScreen extends React.Component {
     constructor(){
@@ -17,28 +15,23 @@ export default class TransactionScreen extends React.Component {
     getCameraPermissions = async () =>{
       const {status} = await Permissions.askAsync(Permissions.CAMERA);
       
-      this.setState({
-        /*status === "granted" is true when user has granted permission
-          status === "granted" is false when user has not granted the permission
-        */
-        hasCameraPermissions: status === "granted",
-        buttonState: 'clicked',
-        scanned: false
-      });
+     
+        
+        
+        
     }
 
     handleBarCodeScanned = async({type, data})=>{
-      this.setState({
-        scanned: true,
-        scannedData: data,
-        buttonState: 'normal'
-      });
+     
+        
+        
+        
     }
 
     render() {
-      const hasCameraPermissions = this.state.hasCameraPermissions;
-      const scanned = this.state.scanned;
-      const buttonState = this.state.buttonState;
+     
+        
+        
 
       if (buttonState === "clicked" && hasCameraPermissions){
         return(
